0f56a682aa2c03cb61095c57918e3a57b22d724a
Nikita Gvozdikov <72789476+kik-OS@users.noreply.github.com>
2021-03-04 10:03:36 +0300

    Initial Commit

---
+++ Varka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Varka
+//
+//  Created by Никита Гвоздиков on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
